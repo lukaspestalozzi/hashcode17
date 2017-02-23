7545bd4c31d12f65465bc085ff49534bbf49078e
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2017-02-23 21:53:21 +0100

    add dict

--- objects.py
+++ objects.py
@@ -15,4 +15,4 @@ CacheLatency = namedtuple("CacheLatency", ["cid", "latency"])
 
 Request = namedtuple("Request", ["video", "amount", "eid"])
 
-EndPoint = namedtuple("EndPoint", ["eid", "latence_to_dc", "cache_latencies", "requests"])
+EndPoint = namedtuple("EndPoint", ["eid", "latence_to_dc", "cache_latencies", "cache_latencies_dict", "requests"])
