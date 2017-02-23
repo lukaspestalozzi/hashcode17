7dd9994a9be5f828cc066301ba89eccb12010ac7
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2017-02-23 15:45:39 +0100

    Enhance boilerplate code

--- practice/pizza.py
+++ practice/pizza.py
@@ -1,10 +1,23 @@
 import fileinput
 
-# Use 'cat input.in | python thisscript.py' to execute
+'''
+    Use 'cat input.in | python thisscript.py' to execute
+'''
+
+# This function solves the problem
+def solveproblem(r, c, l, h, data):
+    # Code extremely efficient solution here
+    print(data)
+
+# read the file
 for line in fileinput.input():
     if fileinput.isfirstline():
         # Save problem parameters
         r, c, l, h = list(map(int, line.split(" ")))
+        # create some data structures for problem data
+        data = []
     else:
-        # Solve Problem
-        print(r, c, l, h)
+        # read input
+        data.append(line.rstrip('\n'))
+
+solveproblem(r, c, l, h, data)
