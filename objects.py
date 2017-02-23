5af9dac3f94dac389bed23a8224237c4052dbf9d
Lukas <lukas.pestalozzi@gmail.com>
2017-02-23 19:35:38 +0100

    added vid to videos

--- objects.py
+++ objects.py
@@ -1,11 +1,11 @@
 from collections import namedtuple
 
-Video = namedtuple("Video", ["size"])
+Video = namedtuple("Video", ["vid", "size"])
 
 Cache = namedtuple("Cache", ["cid", "capacity", "videos"])
 
 CacheLatency = namedtuple("CacheLatency", ["cid", "latency"])
 
-Request = namedtuple("Request", ["vid", "amount", "eid"])
+Request = namedtuple("Request", ["video", "amount", "eid"])
 
 EndPoint = namedtuple("EndPoint", ["eid", "latence_to_dc", "cache_latencies", "caches", "requests"])
