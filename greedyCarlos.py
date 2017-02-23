b4f80f4de8810da7b9f4c7c01cab9d6f10e10ff0
Carlos Malanche <carlos.malancheflores@epfl.ch>
2017-02-23 20:17:01 +0100

    Finished latency per video function

--- greedyCarlos.py
+++ greedyCarlos.py
@@ -5,10 +5,11 @@ def getLatency(videoID,endpointID, caches, videos, endpoints):
     #First we push the latency from the data center
     latencies.append(endpoints[endpointID].latence_to_dc)
     #Now we query the latencies per cache server, we add nothing if the video is not cached
-    for cache
-    return result
-
+    for cacheServer in caches:
+        #We check if the video is cached in a server
+        if(videoID in cacheServer.videos):
+            latencies.append(endpoints[endpointID].cache_latencies[cacheServer.cid].latency)
+    #Worst case it returns latency to the DC
+    return min(latencies)
 
 def solve(caches, endpoints, requests, videos):
-    
-    
