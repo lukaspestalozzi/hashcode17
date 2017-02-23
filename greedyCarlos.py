c6ef45cf8398f2c76309f2c490cc055bba682fbc
Carlos Malanche <carlos.malancheflores@epfl.ch>
2017-02-23 20:23:33 +0100

    Added total latency savings function

--- greedyCarlos.py
+++ greedyCarlos.py
@@ -1,6 +1,7 @@
 from objects import *
 
 def getLatency(videoID,endpointID, caches, videos, endpoints):
+    """Calculates latency of one video"""
     latencies = []
     #First we push the latency from the data center
     latencies.append(endpoints[endpointID].latence_to_dc)
@@ -11,5 +12,13 @@ def getLatency(videoID,endpointID, caches, videos, endpoints):
             latencies.append(endpoints[endpointID].cache_latencies[cacheServer.cid].latency)
     #Worst case it returns latency to the DC
     return min(latencies)
+
+def getTotalLatencySavings(caches, videos, endpoints):
+    """Calculates total latency savings"""
+    result = 0
+    for endpoint in endpoints:
+        for request in endpoint.requests:
+            result += (endpoint.latence_to_dc - getLatency(request.video,endpoint.eid, caches, videos, endpoints)) * request.amount
+    return result
 
 def solve(caches, endpoints, requests, videos):
