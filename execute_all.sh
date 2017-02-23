356082d07812c0cf36821f04e993010a27341983
Joey Zenhäusern <joey.zenhausern@epfl.ch>
2017-02-23 22:07:44 +0100

    output files

--- execute_all.sh
+++ execute_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 zip submission.zip *.py &
-cat kittens.in | python3 cache.py &
-cat trending_today.in | python3 cache.py &
-cat me_at_the_zoo.in | python3 cache.py &
-cat videos_worth_spreading.in | python3 cache.py &
+cat kittens.in | python3 cache.py > outputs/kittens.out &
+cat trending_today.in | python3 cache.py > outputs/trending_today.out &
+cat me_at_the_zoo.in | python3 cache.py > outputs/me_at_the_zoo.out &
+cat videos_worth_spreading.in | python3 cache.py > outputs/videos_worth_spreading.out &
 
 wait
 echo "Done!"
