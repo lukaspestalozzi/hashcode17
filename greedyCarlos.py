71e440cfaafaaa080320685d27149287b25af667
Carlos Malanche <carlos.malancheflores@epfl.ch>
2017-02-23 19:33:11 +0100

    Less retarded format for greedyCarlos

--- greedyCarlos.py
+++ greedyCarlos.py
@@ -1,23 +1,5 @@
-import fileinput
+from objects import *
 
-'''
-    Use 'cat input.in | python thisscript.py' to execute
-'''
-
-# This function solves the problem
-def solveproblem(r, c, l, h, data):
-    # Code extremely efficient solution here
-    print(data)
-
-# read the file
-for line in fileinput.input():
-    if fileinput.isfirstline():
-        # Save problem parameters
-        r, c, l, h = list(map(int, line.split(" ")))
-        # create some data structures for problem data
-        data = []
-    else:
-        # read input
-        data.append(line.rstrip('\n'))
-
-solveproblem(r, c, l, h, data)
+def solve(caches, endpoints, requests):
+    sortedEndpoints
+    
