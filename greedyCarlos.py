c626a47efe087335288dd554e0c7920b5ff137e1
Carlos Malanche <carlos.malancheflores@epfl.ch>
2017-02-23 20:04:28 +0100

    Started latency savings calculation

--- greedyCarlos.py
+++ greedyCarlos.py
@@ -1,5 +1,14 @@
 from objects import *
 
-def solve(caches, endpoints, requests):
-    sortedEndpoints
+def getLatency(videoID,endpointID, caches, videos, endpoints):
+    latencies = []
+    #First we push the latency from the data center
+    latencies.append(endpoints[endpointID].latence_to_dc)
+    #Now we query the latencies per cache server, we add nothing if the video is not cached
+    for cache
+    return result
+
+
+def solve(caches, endpoints, requests, videos):
+    
     
